2dbbf4fc7c3d6b078c4803a32b7d0166dcb66c4d
Eleuthur <105822177+Eleuthur@users.noreply.github.com>
2024-02-15 15:19:18 +0000

    Creates number grid based on user entry with highlight on hover

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.querySelector('#container');
 const square = document.createElement('div');
 
-let number = 5;
+let number = 256;
 
 function createGrid(number) {
-    for (let i=0; i<number; i++) {
+    const screenSize = 960;
+    const squareSize = 960 / number;
+
+    for (let i=0; i<number * number; i++) {
         square.setAttribute("id", `DIV${i}`);
         square.setAttribute("class", "grid");
+        square.setAttribute('style', `width: ${squareSize}px; height: ${squareSize}px`);
         container.appendChild(square.cloneNode(true));
     }
 }
 
-createGrid(number);
+const button = document.querySelector('#sizeButton');
+button.addEventListener('click', () => {
+    container.textContent = "";
+    createGrid(parseInt(prompt("Enter the grid size")));
+});
